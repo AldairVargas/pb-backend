42f56c5f3972b59da459be3d5aa35725298bef58
AldairVargas <aldair.vargas@entrypay.net>
2025-04-09 23:41:57 -0600

    Add routes protection with jwt

--- src/routes/roles.routes.js
+++ src/routes/roles.routes.js
@@ -2,12 +2,13 @@ import express from "express";
 import { getRoles, getRoleById, createRole, updateRole } from "../controllers/role.controller.js";
 import { roleValidator } from "../middleware/validators/role.validator.js";
 import { validate } from "../middleware/validate.js";
+import { verifyToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/roles", getRoles);
-router.get("/roles/:id", getRoleById);
-router.post("/roles", roleValidator, validate, createRole);
-router.put("/roles/:id", roleValidator, validate, updateRole);
+router.get("/roles", verifyToken, getRoles);
+router.get("/roles/:id", verifyToken, getRoleById);
+router.post("/roles", verifyToken, roleValidator, validate, createRole);
+router.put("/roles/:id", verifyToken, roleValidator, validate, updateRole);
 
 export default router;
